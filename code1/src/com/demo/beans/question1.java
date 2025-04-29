8487395c01546f8ea15dc0ce2e2a64758eb31847
RajeshwariKhade <rajeshwarikhade49@gmail.com>
2025-04-29 23:52:47 +0530

    Your commit message here

---
+++ code1/src/com/demo/beans/question1.java
@@ -0,0 +1,117 @@
+//combination sum with at least one even number
+
+
+
+
+
+package com.demo.beans;
+
+import java.util.Arrays;
+
+public class question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int [] arr= {2,5,3,7};
+			//int target=8;
+		//	System.out.println(candidates[1]);
+			
+			for(int i=0;i<arr.length;i++) {
+				Arrays.sort(arr);
+				System.out.println(arr[i]);
+			}
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//input:
+//	candidates=[2,3,5,7]
+//	target=8
+//
+//output:
+//	[2,2,2,2]
+//	[2,3,3]
+
---
+++ code1/src/com/demo/beans/question2.java
@@ -0,0 +1,132 @@
+
+//group words by anagrams
+
+
+
+
+
+
+package com.demo.beans;
+
+import java.util.Arrays;
+
+public class question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   String [] str1= {"eat","tea","tan","ate","nat","bat"};
+	   for(int i=0;i<str1.length;i++) {
+		  
+		   Arrays.sort(str1);
+		  
+		   System.out.println(str1[i]);
+		   
+		   
+		   
+	   }
+		
+		
+
+	}
+
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * group words by anagrams
+ * words=["eat","tea","tan","ate","nat","bat"]
+ * 
+ * output:
+ * 
+ * ["eat","tea","ate"],
+ * ["tan","nat"],
+ * ["bat"]
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
